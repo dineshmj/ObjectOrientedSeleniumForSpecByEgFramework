881e177a89022691c7d86d5ff9f240dfbf610bc6
dineshmj <dineshmj@yahoo.com>
2025-06-17 23:12:41 +0530

    Custom control for HTML Tag info size corrected.

--- Core/WebUIPageStudio/UIControlHtmlTagMapperControl.Designer.cs
+++ Core/WebUIPageStudio/UIControlHtmlTagMapperControl.Designer.cs
@@ -49,10 +49,9 @@
 			// nOfTotalLabel
 			// 
 			nOfTotalLabel.Font = new Font ("Segoe UI Black", 10F, FontStyle.Bold, GraphicsUnit.Point,  0);
-			nOfTotalLabel.Location = new Point (7, 6);
-			nOfTotalLabel.Margin = new Padding (2, 0, 2, 0);
+			nOfTotalLabel.Location = new Point (10, 10);
 			nOfTotalLabel.Name = "nOfTotalLabel";
-			nOfTotalLabel.Size = new Size (84, 60);
+			nOfTotalLabel.Size = new Size (120, 100);
 			nOfTotalLabel.TabIndex = 0;
 			nOfTotalLabel.TextAlign = ContentAlignment.MiddleCenter;
 			// 
@@ -60,10 +59,9 @@
 			// 
 			htmlTagNameLabel.BackColor = Color.LightCyan;
 			htmlTagNameLabel.Font = new Font ("Segoe UI", 9F, FontStyle.Underline, GraphicsUnit.Point,  0);
-			htmlTagNameLabel.Location = new Point (102, 12);
-			htmlTagNameLabel.Margin = new Padding (2, 0, 2, 0);
+			htmlTagNameLabel.Location = new Point (146, 20);
 			htmlTagNameLabel.Name = "htmlTagNameLabel";
-			htmlTagNameLabel.Size = new Size (280, 22);
+			htmlTagNameLabel.Size = new Size (400, 37);
 			htmlTagNameLabel.TabIndex = 1;
 			htmlTagNameLabel.Text = "HTML Tag Name";
 			// 
@@ -71,20 +69,18 @@
 			// 
 			htmlTagNameValueLabel.BorderStyle = BorderStyle.FixedSingle;
 			htmlTagNameValueLabel.Font = new Font ("Consolas", 10F, FontStyle.Bold, GraphicsUnit.Point,  0);
-			htmlTagNameValueLabel.Location = new Point (105, 36);
-			htmlTagNameValueLabel.Margin = new Padding (2, 0, 2, 0);
+			htmlTagNameValueLabel.Location = new Point (150, 60);
 			htmlTagNameValueLabel.Name = "htmlTagNameValueLabel";
-			htmlTagNameValueLabel.Size = new Size (281, 22);
+			htmlTagNameValueLabel.Size = new Size (401, 35);
 			htmlTagNameValueLabel.TabIndex = 2;
 			htmlTagNameValueLabel.TextAlign = ContentAlignment.MiddleLeft;
 			// 
 			// previewPictureBox
 			// 
 			previewPictureBox.BorderStyle = BorderStyle.FixedSingle;
-			previewPictureBox.Location = new Point (1050, 6);
-			previewPictureBox.Margin = new Padding (2, 2, 2, 2);
+			previewPictureBox.Location = new Point (1500, 10);
 			previewPictureBox.Name = "previewPictureBox";
-			previewPictureBox.Size = new Size (211, 88);
+			previewPictureBox.Size = new Size (301, 135);
 			previewPictureBox.TabIndex = 3;
 			previewPictureBox.TabStop = false;
 			// 
@@ -94,10 +90,9 @@
 			mappedControlNameValueLabel.BorderStyle = BorderStyle.FixedSingle;
 			mappedControlNameValueLabel.Font = new Font ("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point,  0);
 			mappedControlNameValueLabel.ForeColor = Color.Lime;
-			mappedControlNameValueLabel.Location = new Point (392, 36);
-			mappedControlNameValueLabel.Margin = new Padding (2, 0, 2, 0);
+			mappedControlNameValueLabel.Location = new Point (560, 60);
 			mappedControlNameValueLabel.Name = "mappedControlNameValueLabel";
-			mappedControlNameValueLabel.Size = new Size (281, 22);
+			mappedControlNameValueLabel.Size = new Size (401, 35);
 			mappedControlNameValueLabel.TabIndex = 2;
 			mappedControlNameValueLabel.TextAlign = ContentAlignment.MiddleLeft;
 			// 
@@ -105,10 +100,9 @@
 			// 
 			mappedControlNameLabel.BackColor = Color.LightCyan;
 			mappedControlNameLabel.Font = new Font ("Segoe UI", 9F, FontStyle.Underline, GraphicsUnit.Point,  0);
-			mappedControlNameLabel.Location = new Point (392, 12);
-			mappedControlNameLabel.Margin = new Padding (2, 0, 2, 0);
+			mappedControlNameLabel.Location = new Point (560, 20);
 			mappedControlNameLabel.Name = "mappedControlNameLabel";
-			mappedControlNameLabel.Size = new Size (280, 22);
+			mappedControlNameLabel.Size = new Size (400, 37);
 			mappedControlNameLabel.TabIndex = 1;
 			mappedControlNameLabel.Text = "Mapped OOSF Control";
 			// 
@@ -116,20 +110,18 @@
 			// 
 			backgroundLabel.BackColor = Color.LightCyan;
 			backgroundLabel.BorderStyle = BorderStyle.Fixed3D;
-			backgroundLabel.Location = new Point (98, 6);
-			backgroundLabel.Margin = new Padding (2, 0, 2, 0);
+			backgroundLabel.Location = new Point (140, 10);
 			backgroundLabel.Name = "backgroundLabel";
-			backgroundLabel.Size = new Size (945, 86);
+			backgroundLabel.Size = new Size (1350, 133);
 			backgroundLabel.TabIndex = 4;
 			// 
 			// pageModelPropertyNameLabel
 			// 
 			pageModelPropertyNameLabel.BackColor = Color.LightCyan;
 			pageModelPropertyNameLabel.Font = new Font ("Segoe UI", 9F, FontStyle.Underline, GraphicsUnit.Point,  0);
-			pageModelPropertyNameLabel.Location = new Point (679, 12);
-			pageModelPropertyNameLabel.Margin = new Padding (2, 0, 2, 0);
+			pageModelPropertyNameLabel.Location = new Point (970, 20);
 			pageModelPropertyNameLabel.Name = "pageModelPropertyNameLabel";
-			pageModelPropertyNameLabel.Size = new Size (280, 22);
+			pageModelPropertyNameLabel.Size = new Size (400, 37);
 			pageModelPropertyNameLabel.TabIndex = 1;
 			pageModelPropertyNameLabel.Text = "Enter Page Model Property Name here";
 			// 
@@ -139,20 +131,18 @@
 			pageModelPropertyNameTextBox.BorderStyle = BorderStyle.FixedSingle;
 			pageModelPropertyNameTextBox.Font = new Font ("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point,  0);
 			pageModelPropertyNameTextBox.ForeColor = Color.White;
-			pageModelPropertyNameTextBox.Location = new Point (679, 36);
-			pageModelPropertyNameTextBox.Margin = new Padding (2, 2, 2, 2);
+			pageModelPropertyNameTextBox.Location = new Point (970, 60);
 			pageModelPropertyNameTextBox.Name = "pageModelPropertyNameTextBox";
-			pageModelPropertyNameTextBox.Size = new Size (281, 26);
+			pageModelPropertyNameTextBox.Size = new Size (401, 36);
 			pageModelPropertyNameTextBox.TabIndex = 0;
 			pageModelPropertyNameTextBox.TextChanged += pageModelPropertyNameTextBox_TextChanged;
 			// 
 			// nameOkPictureBox
 			// 
 			nameOkPictureBox.Image = Resources.StudioResources.Ok;
-			nameOkPictureBox.Location = new Point (966, 36);
-			nameOkPictureBox.Margin = new Padding (2, 2, 2, 2);
+			nameOkPictureBox.Location = new Point (1380, 60);
 			nameOkPictureBox.Name = "nameOkPictureBox";
-			nameOkPictureBox.Size = new Size (25, 22);
+			nameOkPictureBox.Size = new Size (36, 37);
 			nameOkPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
 			nameOkPictureBox.TabIndex = 6;
 			nameOkPictureBox.TabStop = false;
@@ -161,10 +151,9 @@
 			// 
 			nameOkLabel.BackColor = Color.LightCyan;
 			nameOkLabel.Font = new Font ("Segoe UI", 9F, FontStyle.Underline, GraphicsUnit.Point,  0);
-			nameOkLabel.Location = new Point (962, 12);
-			nameOkLabel.Margin = new Padding (2, 0, 2, 0);
+			nameOkLabel.Location = new Point (1374, 20);
 			nameOkLabel.Name = "nameOkLabel";
-			nameOkLabel.Size = new Size (70, 22);
+			nameOkLabel.Size = new Size (100, 37);
 			nameOkLabel.TabIndex = 1;
 			nameOkLabel.Text = "Name OK?";
 			// 
@@ -177,9 +166,10 @@
 			// 
 			doNotInitializeInConstructorCheckBox.AutoSize = true;
 			doNotInitializeInConstructorCheckBox.BackColor = Color.LightCyan;
-			doNotInitializeInConstructorCheckBox.Location = new Point (679, 72);
+			doNotInitializeInConstructorCheckBox.Location = new Point (970, 110);
+			doNotInitializeInConstructorCheckBox.Margin = new Padding (4, 5, 4, 5);
 			doNotInitializeInConstructorCheckBox.Name = "doNotInitializeInConstructorCheckBox";
-			doNotInitializeInConstructorCheckBox.Size = new Size (187, 19);
+			doNotInitializeInConstructorCheckBox.Size = new Size (278, 29);
 			doNotInitializeInConstructorCheckBox.TabIndex = 7;
 			doNotInitializeInConstructorCheckBox.Text = "&Do not initialize in Constructor";
 			whyNameNotOkTooltip.SetToolTip (doNotInitializeInConstructorCheckBox, "Check this if your UI element comes as a result of a User activity (e.g. clicking accordion, mandatory field message, etc.)");
@@ -188,7 +178,7 @@
 			// 
 			// UIControlHtmlTagMapperControl
 			// 
-			AutoScaleDimensions = new SizeF (7F, 15F);
+			AutoScaleDimensions = new SizeF (10F, 25F);
 			AutoScaleMode = AutoScaleMode.Font;
 			Controls.Add (doNotInitializeInConstructorCheckBox);
 			Controls.Add (nameOkPictureBox);
@@ -202,9 +192,8 @@
 			Controls.Add (htmlTagNameLabel);
 			Controls.Add (nOfTotalLabel);
 			Controls.Add (backgroundLabel);
-			Margin = new Padding (2, 2, 2, 2);
 			Name = "UIControlHtmlTagMapperControl";
-			Size = new Size (1267, 98);
+			Size = new Size (1810, 153);
 			((System.ComponentModel.ISupportInitialize) previewPictureBox).EndInit ();
 			((System.ComponentModel.ISupportInitialize) nameOkPictureBox).EndInit ();
 			ResumeLayout (false);
